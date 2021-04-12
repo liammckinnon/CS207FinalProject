4373519cbe55a747be3aaf95a621f01f90012106
liammckinnon <78987271+liammckinnon@users.noreply.github.com>
2021-04-11 20:43:20 -0600

    Add files via upload

---
+++ 1chnl_plant_water_v2/1chnl_plant_water_v2.ino
@@ -0,0 +1,48 @@
+int Relay1 = 2;
+int Snsr = A0;
+float value1 = 0;
+#define FLOAT_SENSOR 3
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Relay1, OUTPUT);
+  pinMode(Snsr, INPUT);
+  pinMode(FLOAT_SENSOR, INPUT_PULLUP);
+
+  digitalWrite(Relay1, HIGH);
+  delay(500);
+}
+void loop() {
+  if (digitalRead(FLOAT_SENSOR) == HIGH)
+  {
+    Serial.println("float up");
+    RunPump();
+  }
+else
+{
+  Serial.println("Float down");
+  delay (1000);
+}
+
+}
+void RunPump()
+{
+  Serial.print("MOISTURE LEVEL:");
+  int ML = analogRead(Snsr);
+  Serial.println(ML);
+  if (ML > 530)
+  {
+    digitalWrite(Relay1, LOW);
+    Serial.println("watering");
+    delay(1000);
+    digitalWrite(Relay1, HIGH);
+  }
+  else
+  {
+
+    digitalWrite(Relay1, HIGH);
+  }
+  Serial.println();
+  delay(1000);
+
+}
